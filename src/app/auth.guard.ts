a8d9041e45e25101bbb259d2f031dbc3814fb28b
Sweetysinghcdac <sweetysinghcdac@gmail.com>
2023-09-06 21:52:58 +0530

    first commit

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,36 @@
+import { CanActivateFn } from '@angular/router';
+import { SellerService } from './services/seller.service';
+import { ɵɵinject } from '@angular/core';
+// import { SellerService } from './services/seller.service';
+
+// export class AuthGuard {
+//   constructor(private sellerService:SellerService) {}
+
+// }
+
+export const authGuard: CanActivateFn = (route, state) => {
+    const sellerService =  ɵɵinject(SellerService);
+
+    // Check if the user is logged in using the AuthService
+    
+    // if (sellerService.isSellerLoggedIn) {
+    //   return true; // If logged in, allow access to the route
+    // } else {
+    //   return false; // If not logged in, deny access to the route
+    // }
+    if (localStorage.getItem('seller')) {
+      return true;
+    }else{
+    // return this.sellerService.isSellerLoggedIn;
+    // return sellerService.isSellerLoggedIn;
+    return false
+    
+    }
+};
+
+
+
+
+
+
+
---
+++ src/app/services/seller.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { SignUp } from '../data-type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SellerService {
+  // reloadSeller() {
+  //     throw new Error('Method not implemented.');
+  // }
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false)
+
+  constructor(private http: HttpClient, private router: Router) { }
+  userSignUp(data: SignUp) {
+    this.http.post('http://localhost:3000/seller', data,
+      { observe: 'response' }).subscribe((result) => {
+        this.isSellerLoggedIn.next(true);
+        // console.warn(result)
+        // if (result) {
+
+          localStorage.setItem('seller', JSON.stringify(result.body))
+          this.router.navigate(['seller-home'])
+          
+        // }
+      })
+  }
+
+  reloadSeller() {
+    if (localStorage.getItem('seller')) {
+      this.isSellerLoggedIn.next(true);
+      this.router.navigate(['seller-home'])
+    }
+  }
+  
+}
